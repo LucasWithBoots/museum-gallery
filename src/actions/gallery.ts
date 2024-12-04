bb4848bb694078b9f9d53f4091167c6291c77549
Lucas Ferrari <lucas.carrijoferrari@gmail.com>
2024-12-04 10:30:00 -0300

    chore: prettier

--- src/actions/gallery.ts
+++ src/actions/gallery.ts
@@ -3,21 +3,21 @@
 import { GALLERY_GET } from "@/functions/api";
 
 interface GalleryItem {
-  id: number;
-  title: string;
-  description: string;
-  image_id: string;
+    id: number;
+    title: string;
+    description: string;
+    image_id: string;
 }
 
 interface GalleryResponse {
-  pagination: object;
-  data: GalleryItem[];
+    pagination: object;
+    data: GalleryItem[];
 }
 
 export async function galleryGet() {
-  const { url } = GALLERY_GET({ page: 1, limit: 10 });
-  const response = await fetch(url);
-  const data: GalleryResponse = await response.json();
+    const { url } = GALLERY_GET({ page: 1, limit: 10 });
+    const response = await fetch(url);
+    const data: GalleryResponse = await response.json();
 
-  return data;
+    return data;
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,14 +3,14 @@ import ArtDisplayed from "@/components/art_displayed";
 import Image from "next/image";
 
 export default async function Home() {
-  const galleries = await galleryGet();
+    const galleries = await galleryGet();
 
-  return (
-    <div>
-      <h1>Teste</h1>
-      {galleries.data.map((gallery) => (
-        <ArtDisplayed title={gallery.title} />
-      ))}
-    </div>
-  );
+    return (
+        <div>
+            <h1>Teste</h1>
+            {galleries.data.map((gallery) => (
+                <ArtDisplayed title={gallery.title} />
+            ))}
+        </div>
+    );
 }
